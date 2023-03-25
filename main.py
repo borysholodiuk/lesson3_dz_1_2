e48c617652682201daa058f4e5b9e8ab7bd13fac
baholodiuk <bory.holodiuk@gmail.com>
2023-03-25 20:42:20 +0200

    not working correct

---
+++ main.py
@@ -0,0 +1,53 @@
+"""
+Домашнє завдання
+Потрібно створити клас Людина(Ім'я, Прізвище, Вік), який має заповнювати клас Студент.
+Потрібно створити клас Студент(Людина(), стипендія(True / False)), який буде заповнювати клас Група.
+Потрібно створити клас Група(назва групи, список студентів).
+"""
+
+
+class Human:
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        self.age = age
+
+    def info(self):
+        return f"Name={self.name} Surname={self.surname} Age={self.age}"
+
+
+class Student:
+    def __init__(self, human: Human, stipend: bool):
+        self.human = Human
+        self.stipend = stipend
+
+    def __str__(self):
+        return f"Student={self.human} Stipend={self.stipend}"
+
+
+class Group:
+    def __init__(self, name: str, students: list):
+        self.name = name
+        self.students = students
+
+    def __str__(self):
+        return f"GroupName={self.name}, Students={self.students}"
+
+    def get_info(self):
+        print(f"GroupName={self.name}")
+        for student in self.students:
+            print(student.human.name)
+
+
+if __name__ == '__main__':
+    andriy = Human(name="Andriy", surname="Gold", age=12)
+    bober = Human(name="Bober", surname="Blodd", age=31)
+    capibar = Human(name="Capibar", surname="Lobzik", age=5)
+
+    student_andriy = Student(andriy, True)
+    student_bober = Student(bober, False)
+    student_capibar = Student(capibar, True)
+    print(andriy, bober, capibar)
+
+    group = Group(name="Group1", students=[student_andriy, student_bober, student_capibar])
+    group.get_info()
